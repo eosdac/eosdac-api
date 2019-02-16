bff602304e29cfbf119b5ed124182adefc6152b1
Michael Yeates <michaeljyeates@gmail.com>
2019-02-16 20:10:04 +0000

    Add rabbitsender

---
+++ rabbitsender.js
@@ -0,0 +1,50 @@
+const Amqp = require('amqplib')
+
+class RabbitSender {
+    constructor (channel, config) {
+        this.channel = channel
+        this.config = config
+    }
+
+    static async init (config) {
+        const conn = await Amqp.connect(config.connectionString)
+        conn.on('error', function (err) {
+            if (err.message !== 'Connection closing') {
+                console.error('[AMQP] conn error', err.message)
+            }
+        })
+        conn.on('close', function () {
+            console.error('[AMQP] closing')
+        })
+
+        const channel = await conn.createChannel()
+        channel.assertQueue('block_range', {durable: true})
+        channel.assertQueue('contract_row', {durable: true})
+
+        return new RabbitSender(channel, config)
+    }
+
+    async send (queue_name, msg) {
+        if (!Buffer.isBuffer(msg)){
+            msg = Buffer.from(msg)
+        }
+
+        return this.channel.sendToQueue(queue_name, msg)
+    }
+
+    async listen (queue_name, cb){
+        this.channel.prefetch(1)
+        // await this.channel.assertQueue(queue_name, {durable: true})
+        this.channel.consume(queue_name, cb, { noAck: false })
+    }
+
+    async ack (job){
+        return this.channel.ack(job)
+    }
+
+    async reject (job){
+        return this.channel.reject(job, true)
+    }
+}
+
+module.exports = RabbitSender
