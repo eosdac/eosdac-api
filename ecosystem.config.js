89f8c7d8ded694381f3ea32b13ed1918768dcd9a
Michael Yeates <michaeljyeates@gmail.com>
2019-03-15 12:47:15 +0000

    Add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+    apps: [
+        {
+            name: "eosdac-filler-jungle",
+            script: "./eosdac-filler.js",
+            node_args: ["--max-old-space-size=8192"],
+            autorestart: true,
+            kill_timeout: 3600,
+            env: {
+                'CONFIG': 'jungle'
+            }
+        },
+        {
+            name: 'eosdac-processor-jungle',
+            script: "./eosdac-processor.js",
+            autorestart: true,
+            env: {
+                'CONFIG': 'jungle'
+            }
+        },
+        {
+            name: 'eosdac-api-jungle',
+            script: "./api.js",
+            autorestart: true,
+            env: {
+                'CONFIG': 'jungle',
+                'SERVER_PORT': 8383,
+                'SERVER_ADDRESS': '0.0.0.0'
+            }
+        },
+
+
+        // MAINNET
+        {
+            name: "eosdac-filler-mainnet",
+            script: "./eosdac-filler.js",
+            node_args: ["--max-old-space-size=8192"],
+            autorestart: true,
+            kill_timeout: 3600,
+            env: {
+                'CONFIG': 'mainnet'
+            }
+        },
+        {
+            name: 'eosdac-processor-mainnet',
+            script: "./eosdac-processor.js",
+            autorestart: true,
+            env: {
+                'CONFIG': 'mainnet'
+            }
+        },
+        {
+            name: 'eosdac-api-mainnet',
+            script: "./api.js",
+            autorestart: true,
+            env: {
+                'CONFIG': 'mainnet',
+                'SERVER_PORT': 8382,
+                'SERVER_ADDRESS': '0.0.0.0'
+            }
+        }
+    ]
+};
