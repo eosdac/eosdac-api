cedb16f42cebbbfb872f69885fdcd4e28e5f8927
Michael Yeates <michaeljyeates@gmail.com>
2019-03-17 13:46:10 +0000

    Fix getRestartBlock

--- functions.js
+++ functions.js
@@ -14,14 +14,22 @@ function loadConfig(){
 async function getRestartBlock(){
     const config = loadConfig()
     const mongo = await connectMongo(config)
+    console.log(`Getting restart block from ${config.mongo.dbName}`)
     const coll_actions = mongo.db(config.mongo.dbName).collection('actions')
     const coll_contract_rows = mongo.db(config.mongo.dbName).collection('contract_rows')
+
+    let actions_block = 1, contract_rows_block = 1
 
     const res_actions = await coll_actions.find({}).sort({block_num:-1}).limit(1).next()
     const res_contract_rows = await coll_contract_rows.find({}).sort({block_num:-1}).limit(1).next()
 
-    const actions_block = res_actions.block_num
-    const contract_rows_block = res_contract_rows.block_num
+    if (res_actions) {
+        actions_block = res_actions.block_num
+    }
+    if (res_contract_rows){
+        contract_rows_block = res_contract_rows.block_num
+    }
+
 
     const block = Math.min(actions_block, contract_rows_block)
 
