bdef0dcdb97f66fb5a09696df0560d311c0e0cbf
Michael Yeates <michaeljyeates@gmail.com>
2019-03-19 22:30:45 +0000

    Add documentation endpoint

--- api.js
+++ api.js
@@ -1,9 +1,13 @@
+const openApi = require('./open-api');
+
 const autoload = require('fastify-autoload');
 const path = require('path');
+const oas = require('fastify-oas')
 const fastify = require('fastify')({
     ignoreTrailingSlash: true,
     trustProxy: true
 });
+fastify.register(oas, openApi.options);
 fastify.register(autoload, {
     dir: path.join(__dirname, 'api-handlers'),
     options: {
@@ -12,11 +16,16 @@ fastify.register(autoload, {
 });
 
 
+fastify.ready().then(async () => {
+    console.log(`Started API server with config ${process.env.CONFIG} on ${process.env.SERVER_ADDR||'127.0.0.1'}:${process.env.SERVER_PORT}`);
+    await fastify.oas();
+}, (err) => {
+    console.error('Error starting API', err)
+});
 
 (async () => {
     try {
-        await fastify.listen(process.env.SERVER_PORT, process.env.SERVER_ADDR);
-        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        await fastify.listen(process.env.SERVER_PORT, process.env.SERVER_ADDR)
     } catch (err) {
         fastify.log.error(err);
         console.error(err)
