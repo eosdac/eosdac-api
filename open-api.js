bb217e4cc34cec2e9909252fbf00c6d52f51e7a5
Michael Yeates <michaeljyeates@gmail.com>
2019-03-19 23:06:40 +0000

    Add openapi definition

---
+++ open-api.js
@@ -0,0 +1,15 @@
+exports.options = {
+    routePrefix: '/v1/eosdac/docs',
+    exposeRoute: true,
+    swagger: {
+        info: {
+            title: 'eosDAC API',
+            description: 'API for running DACs',
+            version: '1.0.0'
+        },
+        host: process.env.HOST_NAME,
+        schemes: ['https'],
+        consumes: ['application/json'],
+        produces: ['application/json']
+    }
+};
