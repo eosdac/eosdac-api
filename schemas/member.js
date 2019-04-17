3e492969311d01b4f329c428ee6978c58ab53187
Michael Yeates <michaeljyeates@gmail.com>
2019-04-17 16:18:14 +0300

    Add members api

---
+++ schemas/member.js
@@ -0,0 +1,17 @@
+const memberSchema = {
+    description: 'Get member information',
+    summary: 'Get information about a member, including token balances, profile and votes',
+    tags: ['v1'],
+    querystring: {
+        type: 'object',
+        properties: {
+            "account": {
+                description: 'Account to fetch, you can supply multiple accounts by separating them with a comma',
+                type: 'string'
+            }
+        },
+        required: []
+    }
+};
+
+module.exports = {GET: memberSchema};
