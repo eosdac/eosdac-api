60bcd045e4ecd658cb4cafe46a7b05a7aa4ac923
Michael Yeates <michaeljyeates@gmail.com>
2019-04-25 22:08:00 +0100

    Update file paths

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,6 @@
-const ActionHandler = require('./handlers/action-handler');
-const TraceHandler = require('./handlers/trace-handler');
-const DeltaHandler = require('./handlers/delta-handler');
+const ActionHandler = require('./action-handler');
+const TraceHandler = require('./trace-handler');
+const DeltaHandler = require('./delta-handler');
 
 
 module.exports = {ActionHandler, TraceHandler, DeltaHandler};
