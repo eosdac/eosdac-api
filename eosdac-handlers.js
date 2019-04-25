143d8a77fca2f4df9d199aa4c1c426b538d9bf7a
Michael Yeates <michaeljyeates@gmail.com>
2019-04-25 12:17:39 +0100

    Remove old files

--- eosdac-handlers.js
+++
@@ -1,7 +0,0 @@
-const ActionHandler = require('./handlers/action-handler');
-const TraceHandler = require('./handlers/trace-handler');
-const DeltaHandler = require('./handlers/delta-handler');
-
-
-module.exports = {ActionHandler, TraceHandler, DeltaHandler};
-
