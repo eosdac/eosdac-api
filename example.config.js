46ce72627a579a3e1e37214ba462882748ee0382
Luke Stokes <luke.stokes@gmail.com>
2019-04-26 11:42:08 -0400

    Adding the msig contract to the list of contracts we're watching

--- example.config.js
+++ example.config.js
@@ -11,7 +11,7 @@ module.exports = {
         connectionString: 'amqp://user:pass@host/vhost'
     },
     eos: {
-        contracts: ['kasdactokens', 'dacelections', 'eosdacdoshhq', 'dacmultisigs'],
+        contracts: ['kasdactokens', 'dacelections', 'eosdacdoshhq', 'dacmultisigs', 'eosio.msig'],
         chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473",
         endpoint: 'http://jungle2.eosdac.io:8882',
         wsEndpoint: 'ws://jungle2.eosdac.io:8080',
