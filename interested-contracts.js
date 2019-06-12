23868a49993b95692e9875c99c88af84e6667a9e
Michael Yeates <michaeljyeates@gmail.com>
2019-06-12 15:43:59 +0100

    Extract all auth, msig and custodian contracts for use by api

--- interested-contracts.js
+++ interested-contracts.js
@@ -1,5 +1,5 @@
 
-const {Api, JsonRpc, Serialize} = require('eosjs');
+const {Api, JsonRpc} = require('eosjs');
 const {TextDecoder, TextEncoder} = require('text-encoding');
 const fetch = require('node-fetch');
 
@@ -9,18 +9,19 @@ class InterestedContracts {
         this.config = config;
         this.db = db;
         this.interested_timeout = null;
+        this.interested_contracts = new Set();
+        this._auth_accounts = new Map();
+        this._msig_contracts = new Map();
+        this._custodian_contracts = new Map();
 
-        const rpc = new JsonRpc(config.eos.endpoint, {fetch});
-        const api = new Api({
-            rpc,
+        this.rpc = new JsonRpc(config.eos.endpoint, {fetch});
+        this.api = new Api({
+            rpc: this.rpc,
             signatureProvider: null,
             chainId: config.chainId,
             textDecoder: new TextDecoder(),
             textEncoder: new TextEncoder(),
         });
-
-        this.api = api;
-        this.rpc = rpc;
     }
 
     add(account, dac_name) {
@@ -34,6 +35,10 @@ class InterestedContracts {
 
     async reload() {
         this.interested_contracts = new Set();
+        this._msig_contracts = new Map();
+        this._custodian_contracts = new Map();
+        this._auth_accounts = new Map();
+
         const res = await this.rpc.get_table_rows({
             code: this.config.eos.dacDirectoryContract,
             scope: this.config.eos.dacDirectoryContract,
@@ -44,8 +49,30 @@ class InterestedContracts {
             row.accounts.forEach((acnt) => {
                 // console.log(`Adding ${acnt} to set`);
                 this.interested_contracts.add(acnt.value);
+
+                if (acnt.key === 3){ // multisig
+                    this._msig_contracts.set(row.dac_name, acnt.value);
+                }
+                else if (acnt.key === 2){ // custodian
+                    this._custodian_contracts.set(row.dac_name, acnt.value);
+                }
+                else if (acnt.key === 0){ // auth
+                    this._auth_accounts.set(row.dac_name, acnt.value);
+                }
             });
         });
+    }
+
+    auth_accounts() {
+        return this._auth_accounts;
+    }
+
+    custodian_contracts() {
+        return this._custodian_contracts;
+    }
+
+    msig_contracts() {
+        return this._msig_contracts;
     }
 
     has(value) {
