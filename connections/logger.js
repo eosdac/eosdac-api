3bf1925e72564a00daf9cf88dfec5b1a4110dfa7
Michael Yeates <michaeljyeates@gmail.com>
2019-08-12 22:15:26 +0100

    Do not override log

--- connections/logger.js
+++ connections/logger.js
@@ -30,7 +30,7 @@ const logger = ((service='undefined-service', config) => {
 
 
     // To make compatible with fastify logger
-    _l.log = () => {_l.info(arguments[0])};
+    // _l.log = () => {_l.info(arguments[0])};
     _l.fatal = () => {_l.error(arguments[0])};
     _l.trace = () => {_l.silly(arguments[0])};
 
