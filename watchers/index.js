80f603c212fb75173821a088054d8f72de4eb745
Michael Yeates <michaeljyeates@gmail.com>
2019-09-19 13:53:45 +0100

    Send newperiod notifications to ipc for websocket

--- watchers/index.js
+++ watchers/index.js
@@ -1,7 +1,9 @@
 const multisigProposals = require('./multisig_proposals');
 const workerProposals = require('./worker_proposals');
+const newPeriod = require('./new_period');
 
 module.exports = [
     multisigProposals,
-    workerProposals
+    workerProposals,
+    newPeriod
 ];
