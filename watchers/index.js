7f9a9e7d6776d1770d89ff5b99c245ddd7739913
Michael Yeates <michaeljyeates@gmail.com>
2019-12-15 18:45:09 +0000

    Add watcher for member status

--- watchers/index.js
+++ watchers/index.js
@@ -2,8 +2,10 @@ const multisigProposals = require('./multisig_proposals');
 const workerProposals = require('./worker_proposals');
 const votes = require('./votes');
 const newPeriod = require('./new_period');
+const memberStatus = require('./member_status');
 
 module.exports = [
+    memberStatus,
     multisigProposals,
     workerProposals,
     votes,
