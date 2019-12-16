c1170afd6bbbce22c42daec07c8075471ccc64dc
Michael Yeates <michaeljyeates@gmail.com>
2019-12-16 00:20:44 +0000

    Simplify and remove unused code

--- api-handlers/myDacs.js
+++ api-handlers/myDacs.js
@@ -1,8 +1,9 @@
 const {myDacsSchema} = require('../schemas');
-const {eosTableAtBlock} = require('../eos-table');
 
+async function myDacs(fastify, request) {
+    const db = fastify.mongo.db;
+    const account = request.query.account;
 
-async function getMembershipTypes(account, db){
     const data_query = {
         account
     };
@@ -16,68 +17,7 @@ async function getMembershipTypes(account, db){
         dacs.push(row);
     }
 
-    return dacs;
-}
-
-async function myDacs(fastify, request) {
-
-    return new Promise(async (resolve, reject) => {
-        const dac_config = await request.dac_config();
-        // console.log(dac_config);
-        //console.log(dac_directory._custodian_contracts);
-        const dac_id = request.dac();
-
-        const api = fastify.eos.api;
-        const db = fastify.mongo.db;
-        const actions_col = db.collection('actions');
-
-        const account = request.query.account;
-
-        const res = await getMembershipTypes(account, db);
-
-        resolve({results: res, count: res.length});
-
-        // dac_data.map((row) => {
-        //     console.log(row);
-        //     // check for candidate
-        //
-        //  });
-
-
-
-        // console.log(dac_data);
-
-        return;
-
-        fastify.log.info(`Getting DACs for ${account}`);
-
-        try {
-            const query = {
-                "action.name": "memberrege",
-                "action.data.sender": account
-            };
-            const res = await actions_col.find(query);
-
-            const dacs = new Set();
-            res.forEach((row) => {
-                console.log(row);
-                dacs.add(row.action.data.dac_id);
-            }, async () => {
-
-                const results = [];
-
-
-                resolve(results);
-
-            });
-
-        } catch (e) {
-            reject(e);
-        }
-
-    })
-
-
+    return {results: dacs, count: dacs.length};
 }
 
 
