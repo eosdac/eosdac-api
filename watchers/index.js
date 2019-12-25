7cc47a90f648a0f98c063d4656d382a556729ae2
Michael Yeates <michaeljyeates@gmail.com>
2019-12-25 21:16:09 +0000

    Optimise tokens owned

--- watchers/index.js
+++ watchers/index.js
@@ -1,6 +1,7 @@
 const multisigProposals = require('./multisig_proposals');
 const workerProposals = require('./worker_proposals');
 const votes = require('./votes');
+const tokens = require('./tokens');
 const newPeriod = require('./new_period');
 const memberStatus = require('./member_status');
 
@@ -9,5 +10,6 @@ module.exports = [
     multisigProposals,
     workerProposals,
     votes,
+    tokens,
     newPeriod
 ];
---
+++ watchers/tokens.js
@@ -0,0 +1,121 @@
+const connectMongo = require('../connections/mongo');
+const {loadConfig} = require('../functions');
+
+const config = loadConfig();
+
+const {eosTableAtBlock} = require('../eos-table');
+const DacDirectory = require('../dac-directory');
+
+/*
+Maintains a cache of tokens owned
+ */
+
+class TokensHandler {
+
+    constructor() {
+        this.config = config;
+
+        this.dac_config = null;
+
+        this.logger = require('../connections/logger')('watcher-multisig', config.logger);
+    }
+
+    async recalc_tokens(account, db){
+        return new Promise(async (resolve, reject) => {
+
+            const query = {'scope': account, 'table': 'accounts'};
+
+            const collection = db.collection('contract_rows');
+            const cache_collection = db.collection('tokens');
+
+            cache_collection.createIndex({acnt:1, code:1, symbol:1, precision:1}, {unique:true});
+            cache_collection.createIndex({acnt:1}, {background:true});
+
+            collection.find(query, {sort: {block_num: 1}}, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                else if (res) {
+                    const symbols = new Set();
+                    res.count().then((cnt) => {
+                        console.log(`Found ${cnt} records`);
+                        if (cnt){
+                            res.forEach((row) => {
+                                // console.log(row);
+                                if (row.data.balance){
+                                    const [bal, sym] = row.data.balance.split(' ');
+                                    const bal_float = parseFloat(bal);
+                                    if (!isNaN(bal_float) && bal_float > 0){
+                                        const [,d] = bal.split('.');
+                                        const p = (d)?d.length:0;
+
+                                        symbols.add(`${row.code}:${sym}:${p}`);
+                                    }
+                                }
+                            }, () => {
+                                console.log(`Writing ${symbols.size} symbols for ${account}`);
+                                cache_collection.deleteMany({acnt:account}, {}, () => {
+                                    const bulk_writes = [];
+
+                                    if (symbols.size){
+                                        Array.from(symbols).forEach((s) => {
+                                            const [code, symbol, precision] = s.split(':');
+                                            bulk_writes.push({
+                                                updateOne: {
+                                                    filter: {acnt:account, code, symbol, precision:parseInt(precision)},
+                                                    update: {$set:{acnt:account, code, symbol, precision:parseInt(precision)}},
+                                                    upsert: true
+                                                }
+                                            });
+                                        });
+
+                                        // console.log(JSON.stringify(bulk_writes));
+                                        cache_collection.bulkWrite(bulk_writes, {ordered:false}).then((res, err) => {
+                                            if (err){
+                                                reject(err);
+                                            }
+                                            else {
+                                                resolve();
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        resolve();
+                                    }
+                                });
+
+                            });
+                        }
+                    });
+                }
+            });
+        });
+
+    }
+
+    async action({doc, dac_directory, db}) {}
+
+    async delta({doc, dac_directory, db}){
+        if (doc.table === 'accounts'){
+            this.recalc_tokens(doc.scope, db);
+        }
+    }
+
+    async replay() {
+        console.log(`Recalculating token balances`);
+        const db = await connectMongo(config);
+        // this.recalc_tokens('evilmikehere', db);
+        // return;
+        const collection = db.collection('contract_rows');
+        const res = await collection.distinct('scope', {table:'accounts'});
+        const promises = [];
+        res.forEach(account => {
+            promises.push(this.recalc_tokens(account, db));
+        });
+
+        await Promise.all(promises);
+        console.log(`Recalculated ${promises.length} account tokens owned`);
+    }
+}
+
+module.exports = new TokensHandler();
