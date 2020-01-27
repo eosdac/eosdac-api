1e4535a3d51527c8eef0cac5799077cde90d9a96
Michael Yeates <michaeljyeates@gmail.com>
2020-01-27 12:55:42 +0000

    Sort referendums by expiry date

--- api-handlers/referendums.js
+++ api-handlers/referendums.js
@@ -10,14 +10,13 @@ async function getReferendums(fastify, request) {
         const db = fastify.mongo.db;
         const dac_id = request.dac();
 
-        const now = new Date();
         const coll = db.collection('referendums');
         const query = { dac_id };
         const status = request.query.status;
         if (status !== undefined) {
             query.status = status;
         }
-        const res = await coll.find(query);
+        const res = await coll.find(query).sort({expires:-1});
 
         let ret = [];
         res.forEach((row) => {
