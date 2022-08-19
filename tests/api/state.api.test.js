cb14b38a164e3a0a1f6e8710cdac837e7916462c
Ahmad Tirmazi <atirmazi@Ahmads-MacBook-Pro.local>
2022-08-19 12:46:53 +0200

    test: write api tests for /state
    Signed-off-by: Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>

--- tests/api/state.api.test.js
+++ tests/api/state.api.test.js
@@ -1,17 +1,35 @@
 const { createApiTestEnvironment } = require('../environments');
+const { HTTP_METHOD, HTTP_STATUS } = require('../common');
+const { stateSuccessResponse } = require('../fixtures/state.fixture');
 
 const environment = createApiTestEnvironment();
 environment.initialize();
 
+
+// meta
+const Api = {
+  method: HTTP_METHOD.GET,
+  url: "/v1/eosdac/state"
+}
+
 describe('State API Test', () => {
 
-  it('Should return 200', async () => {
-    const response = await environment.server.inject({
-      method: 'GET',
-      url: '/v1/eosdac/state',
-    });
+  it('should return status 200', async () => {
+    const response = await getApiResponse(Api.method, Api.url)
 
-    expect(response.statusCode).toEqual(200);
+    expect(response.statusCode).toEqual(HTTP_STATUS.OK);
+  });
+
+  it('should return current block', async () => {
+    const response = await getApiResponse(Api.method, Api.url)
+
+    expect(JSON.parse(response.body)).toEqual(stateSuccessResponse);
   });
 
 });
+
+
+// helpers
+const getApiResponse = async function (method, url) {
+  return await environment.server.inject({ method, url });
+}
---
+++ tests/common.js
@@ -0,0 +1,10 @@
+const HTTP_METHOD = {
+    GET: "GET"
+}
+
+const HTTP_STATUS = {
+    OK: 200,
+    BAD_REQUEST: 400,
+}
+
+module.exports = { HTTP_METHOD, HTTP_STATUS }
