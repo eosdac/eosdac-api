f391311ef382171bd682accade04df6e300d76e3
rkamysz <radoslaw.kamysz@gmail.com>
2023-02-07 21:21:03 +0100

    resolve

--- src/endpoints/profile/domain/use-cases/__tests__/is-profile-flagged.use-case.unit.test.ts
+++ src/endpoints/profile/domain/use-cases/__tests__/is-profile-flagged.use-case.unit.test.ts
@@ -2,76 +2,83 @@ import 'reflect-metadata';
 
 import { DaoWorldsContract, FlagRepository } from '@alien-worlds/eosdac-api-common';
 import { Failure, Result } from '@alien-worlds/api-core';
+import { Flag, FlagRepository } from '@alien-worlds/eosdac-api-common';
+import { IsProfileFlaggedUseCaseInput, IsProfileFlaggedUseCaseOutput } from 'src/endpoints/profile/data/dtos/profile.dto';
 
 import { Container } from 'inversify';
 import { IsProfileFlaggedUseCase } from '../is-profile-flagged.use-case';
-import { IsProfileFlaggedUseCaseInput } from 'src/endpoints/profile/data/dtos/profile.dto';
 
 /*imports*/
 /*mocks*/
 
-
 let container: Container;
 let useCase: IsProfileFlaggedUseCase;
 const useCaseInput: IsProfileFlaggedUseCaseInput = {
-  dacId: 'string',
-  accounts: ['awtesteroo12'],
-}
+	dacId: 'string',
+	accounts: ['awtesteroo12'],
+};
 const flagRepository = {
-  update: jest.fn(),
-  updateMany: jest.fn(),
-  add: jest.fn(),
-  addOnce: jest.fn(),
-  count: jest.fn(),
-  find: jest.fn(),
-  findOne: jest.fn(),
-  aggregate: jest.fn(),
-  remove: jest.fn(),
-  removeMany: jest.fn(),
+	update: jest.fn(),
+	updateMany: jest.fn(),
+	add: jest.fn(),
+	addOnce: jest.fn(),
+	count: jest.fn(),
+	find: jest.fn(),
+	findOne: jest.fn(),
+	aggregate: jest.fn(),
+	remove: jest.fn(),
+	removeMany: jest.fn(),
 };
 
 describe('Is Profile Flagged Unit tests', () => {
-  beforeAll(() => {
-    container = new Container();
+	beforeAll(() => {
+		container = new Container();
 
-    container
-      .bind<IsProfileFlaggedUseCase>(IsProfileFlaggedUseCase.Token)
-      .to(IsProfileFlaggedUseCase);
-    container
-      .bind<FlagRepository>(FlagRepository.Token)
-      .toConstantValue(flagRepository);
-  });
+		container
+			.bind<IsProfileFlaggedUseCase>(IsProfileFlaggedUseCase.Token)
+			.to(IsProfileFlaggedUseCase);
+		container
+			.bind<FlagRepository>(FlagRepository.Token)
+			.toConstantValue(flagRepository);
+	});
 
-  beforeEach(() => {
-    useCase = container.get<IsProfileFlaggedUseCase>(IsProfileFlaggedUseCase.Token);
-  });
+	beforeEach(() => {
+		useCase = container.get<IsProfileFlaggedUseCase>(
+			IsProfileFlaggedUseCase.Token
+		);
+	});
 
-  afterAll(() => {
-    jest.clearAllMocks();
-    container = null;
-  });
+	afterAll(() => {
+		jest.clearAllMocks();
+		container = null;
+	});
 
-  it('"Token" should be set', () => {
-    expect(IsProfileFlaggedUseCase.Token).not.toBeNull();
-  });
+	it('"Token" should be set', () => {
+		expect(IsProfileFlaggedUseCase.Token).not.toBeNull();
+	});
 
-  it('Should return a failure when ...', async () => {
-    flagRepository.aggregate.mockResolvedValue(Result.withFailure(Failure.fromError("error")))
-    const result = await useCase.execute(useCaseInput);
-    expect(result.isFailure).toBeTruthy();
-  });
+	it('Should return a failure when ...', async () => {
+		flagRepository.aggregate.mockResolvedValue(
+			Result.withFailure(Failure.fromError('error'))
+		);
+		const result = await useCase.execute(useCaseInput);
+		expect(result.isFailure).toBeTruthy();
+	});
 
-  it('should return Array', async () => {
-    const content = [DaoWorldsContract.Actions.Entities.FlagCandidateProfile.fromStruct({
-      cand: 'cand',
-      dac_id: 'dacId',
-      block: true,
-    })]
-    flagRepository.aggregate.mockResolvedValue(Result.withContent(content))
-    const result = await useCase.execute(useCaseInput);
-    expect(result.content).toBeInstanceOf(Array);
-  });
+	it('should return Array', async () => {
+		const content = [
+			FlagCandidateProfile.fromStruct({
+				cand: 'cand',
+				reason: '',
+				reporter: '',
+				block: true,
+				dac_id: 'dacId',
+			}),
+		];
+		flagRepository.aggregate.mockResolvedValue(Result.withContent(content));
+		const result = await useCase.execute(useCaseInput);
+		expect(result.content).toBeInstanceOf(Array);
+	});
 
-  /*unit-tests*/
+	/*unit-tests*/
 });
-
