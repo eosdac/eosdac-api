4e3166ec8cae45862c50f0d656292722acd512fe
Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>
2023-02-20 16:58:15 +0100

    refactor: implement json schema based validator

---
+++ src/validator/ajv-validator.ts
@@ -0,0 +1,48 @@
+import { Request, ValidationResult } from "@alien-worlds/api-core";
+import { ErrorObject } from 'ajv';
+import Ajv2020 from "ajv/dist/2020"
+
+import { Validator } from './validator.interface';
+
+export class AjvValidator implements Validator {
+
+    private constructor(private readonly _ajv: Ajv2020) { }
+
+    public static initialize(): Validator {
+        const validationOptions = {
+            allErrors: true,
+            verbose: true,
+            coerceTypes: true,
+        }
+
+        return new AjvValidator(new Ajv2020(validationOptions));
+    }
+
+    validate(schema: object, data: object): boolean {
+        return this._ajv.validate(schema, data);
+    }
+
+    validateHttpRequest(jsonSchema: object, request: Request<unknown>): ValidationResult {
+        const valid = this.validate(jsonSchema, request)
+
+        return {
+            valid,
+            message: !valid ? "bad request" : "",
+            errors: this.errors,
+        }
+    }
+
+    assert(schema: object, data: object): void {
+        if (!this.validate(schema, data)) {
+            throw new Error(this._ajv.errorsText())
+        }
+    }
+
+    get errors(): string[] {
+        return this._ajv.errors?.map(this.humanizeAjvError) || [];
+    }
+
+    private humanizeAjvError(err: ErrorObject): string {
+        return `${err.instancePath} ${err.message}`
+    }
+}
---
+++ src/validator/validator.interface.ts
@@ -0,0 +1,32 @@
+import { Request, ValidationResult } from '@alien-worlds/api-core';
+export interface Validator {
+    /**
+     * 
+     * @param {Object} jsonSchema - JSON schema object
+     * @param {Object} data - Data object to validate
+     * @returns {boolean} `true` for valid data, otherwise `false`
+     */
+    validate(jsonSchema: object, data: object): boolean;
+
+    /**
+     * 
+     * @param {Object} jsonSchema - JSON schema object
+     * @param {Request} request - HTTP request to validate
+     * @returns {ValidationResult}
+     */
+    validateHttpRequest(jsonSchema: object, request: Request): ValidationResult;
+
+    /**
+     * @param {Object} jsonSchema - JSON schema object
+     * @param {Object} data - Data object to validate
+     * @throws {Error}
+     * @returns {void}
+     */
+    assert(jsonSchema: object, data: object): void;
+
+    /**
+     * 
+     * @returns {string[]} array of user friendly validation error messages 
+     */
+    readonly errors: string[];
+}
