b984296c8e4475a2ddd5a5e785945a9d05c7d6ab
Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>
2023-04-13 13:02:02 +0200

    docs: update postman collection and open api docs

--- src/open-api.ts
+++ src/open-api.ts
@@ -3,7 +3,7 @@ import { FastifyStaticSwaggerOptions } from '@fastify/swagger';
 
 const options: FastifyStaticSwaggerOptions = {
   mode: 'static',
-  routePrefix: `/${config.version}/dao/docs`,
+  routePrefix: `/${config.version}${config.docs.routePrefix}`,
   exposeRoute: true,
   specification: {
     path: './docs/dao-api-oas.yaml',
